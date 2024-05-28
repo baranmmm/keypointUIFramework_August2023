05401b756a85a284fe0d9842a215659b79959c0c
Musab <mcoban03@gmail.com>
2024-05-28 23:05:12 +0100

    28-05

--- src/test/java/talrise/pages/superadmin/SuperAdminCandidateProcessPage.java
+++ src/test/java/talrise/pages/superadmin/SuperAdminCandidateProcessPage.java
@@ -1,5 +1,6 @@
 package talrise.pages.superadmin;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import talrise.pages.CommonPageElements;
